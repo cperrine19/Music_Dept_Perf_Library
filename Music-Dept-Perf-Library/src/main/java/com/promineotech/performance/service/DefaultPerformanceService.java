aa453e6b94b7c6725c087ce908fcf533f7142525
Chelsea Perrine <perrinechelsea@gmail.com>
2022-06-18 21:26:58 -0500

    week14- read description
    check step 7e
    check comments in Default Performance Service
    added to pom (steps 8 and 11)
    skipped steps that required controller

---
+++ Music-Dept-Perf-Library/src/main/java/com/promineotech/performance/service/DefaultPerformanceService.java
@@ -0,0 +1,25 @@
+package com.promineotech.performance.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class DefaultPerformanceService implements PerformanceService{
+	//@Autowired
+	//private PerformanceDao performanceDao;
+	
+	//public List<Performances> fetchPerformances(String date) {
+		//log.info("The fetchPeformances method was called with date", date);
+		//List<Performances> performances = performanceDao.fetchPerformances(date);
+	//if (performances.isEmpty()) {
+	//String msg = String.format("No performances found with date", date);
+	//throw new NoSuchElementException(msg);
+//}
+
+//Collections.sort(performances);
+//return performances;
+}
+
---
+++ Music-Dept-Perf-Library/src/main/java/com/promineotech/performance/service/PerformanceService.java
@@ -0,0 +1,5 @@
+package com.promineotech.performance.service;
+
+public interface PerformanceService {
+
+}
