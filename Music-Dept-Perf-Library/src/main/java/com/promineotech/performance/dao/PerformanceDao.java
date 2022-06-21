06d549c162c3962739af6b14339e62ec1f02b0a5
Chelsea Perrine <perrinechelsea@gmail.com>
2022-06-20 20:33:26 -0500

    Jun20- finishing week 14

---
+++ Music-Dept-Perf-Library/src/main/java/com/promineotech/performance/dao/PerformanceDao.java
@@ -0,0 +1,14 @@
+package com.promineotech.performance.dao;
+
+import java.util.List;
+
+import com.promineotech.performance.entity.Performances;
+
+public class PerformanceDao {
+
+	public List<Performances> fetchPerformances(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
