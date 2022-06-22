0dc25f2b4de0c3a5fb64cddfe481f6437491dce6
Chelsea Perrine <97609385+cperrine19@users.noreply.github.com>
2022-06-21 20:33:39 -0500

    Update Music_Dept_Perf_Library_Data.sql
    data update

--- Music-Dept-Perf-Library/src/test/resources/Music_Dept_Perf_Library_Data.sql
+++ Music-Dept-Perf-Library/src/test/resources/Music_Dept_Perf_Library_Data.sql
@@ -7,31 +7,29 @@ INSERT INTO composer (first_name, last_name) VALUES ('FREDERIC', 'CHOPIN');
 INSERT INTO composer (first_name, last_name) VALUES ('PYOTR', 'TCHAIKOVSKY');
 INSERT INTO composer (first_name, last_name) VALUES ('JOHANNES', 'BRAHMS');
 
-INSERT INTO song_type (performance_type) VALUES ('soprano vocal solo');
-INSERT INTO song_type (performance_type) VALUES ('alto vocal solo');
-INSERT INTO song_type (performance_type) VALUES ('tenor vocal solo');
-INSERT INTO song_type (performance_type) VALUES ('bass vocal solo');
-INSERT INTO song_type (performance_type) VALUES ('vocal duet');
-INSERT INTO song_type (performance_type) VALUES ('vocal trio');
-INSERT INTO song_type (performance_type) VALUES ('vocal quartet');
-INSERT INTO song_type (performance_type) VALUES ('vocal mixed chorus');
+INSERT INTO performance_type (performance_type) VALUES ('soprano vocal solo');
+INSERT INTO performance_type (performance_type) VALUES ('alto vocal solo');
+INSERT INTO performance_type (performance_type) VALUES ('tenor vocal solo');
+INSERT INTO performance_type (performance_type) VALUES ('bass vocal solo');
+INSERT INTO performance_type (performance_type) VALUES ('vocal duet');
+INSERT INTO performance_type (performance_type) VALUES ('vocal trio');
+INSERT INTO performance_type (performance_type) VALUES ('vocal quartet');
+INSERT INTO performance_type (performance_type) VALUES ('vocal mixed chorus');
 
 INSERT INTO performer (artist_name) VALUES ('JLADYMAN');
 INSERT INTO performer (artist_name) VALUES ('CPERRINE');
-INSERT INTO performer (artist_name) VALUES ('MADEUP SCHOOL CHORUS');.
+INSERT INTO performer (artist_name) VALUES ('MADEUP SCHOOL CHORUS');
 
-INSERT INTO performances (date) VALUES ("12-12-2012");
-INSERT INTO performances (date) VALUES ("01-12-2011");
-INSERT INTO performances (date) VALUES ("02-12-2013");
+INSERT INTO performances (performance_date) VALUES ("2017-06-15");
+INSERT INTO performances (performance_date) VALUES ("2017-06-15");
+INSERT INTO performances (performance_date) VALUES ("2018-06-15");
+INSERT INTO performances (performance_date) VALUES ("2019-06-15");
+INSERT INTO performances (performance_date) VALUES ("2020-06-15");
+INSERT INTO performances (performance_date) VALUES ("2020-02-02");
 
-INSERT INTO song_type (performance_type) VALUES ("FluteSolo");
-INSERT INTO song_type (performance_type) VALUES ("HornSolo");
-INSERT INTO song_type (performance_type) VALUES ("SopranoSolo");
-INSERT INTO song_type (performance_type) VALUES ("VocalDuet");
-
-INSERT INTO song_title (title) VALUES ("Nachtmusik");
-INSERT INTO song_title (title) VALUES ("PianoSonataNo11");
-INSERT INTO song_title (title) VALUES ("Elsa'sDream");
+INSERT INTO song_title (song_title) VALUES ("Nachtmusik");
+INSERT INTO song_title (song_title) VALUES ("PianoSonataNo11");
+INSERT INTO song_title (song_title) VALUES ("Elsa'sDream");
 
 
 
