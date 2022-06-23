b09fc678281822a26718949ee8309e818c048037
Chelsea Perrine <perrinechelsea@gmail.com>
2022-06-23 12:51:29 -0500

    june23
    updated last line of sql data
    updated Create Performance entity class based on jeep.orderRequest class but need help here

--- Music-Dept-Perf-Library/src/main/java/com/promineotech/performance/entity/CreatePerformance.java
+++ Music-Dept-Perf-Library/src/main/java/com/promineotech/performance/entity/CreatePerformance.java
@@ -1,5 +1,15 @@
 package com.promineotech.performance.entity;
 
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+
 public class CreatePerformance {
    //did not include bean validation- step 7 in week 16
+	private String performance_name;
+	private String performance_date;
+	private List<String> performances; //not sure about what to use here
 }
